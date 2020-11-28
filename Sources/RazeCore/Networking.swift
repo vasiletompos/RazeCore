3f291d2e0654e556eef4f249779aa6f923e908ac
Tony Tompos <tony.tompos@icloud.com>
2020-11-28 12:39:46 -0800

    Refactored first Networking API to include dependency injection for easier testing

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -7,23 +7,39 @@
 
 import Foundation
 
+protocol NetworkSession {
+	func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkSession {
+	func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+		let task = dataTask(with: url) { data, _, error in
+			completionHandler(data, error)
+		}
+		task.resume()
+	}
+}
+
 extension RazeCore {
+	
 	public class Networking {
+		
 		/// Responsible for handling all networking calls
 		/// - Warning: Must create before using any public APIs
 		public class Manager {
-			public init() {
-				
-			}
+			public init() {}
 			
-			private let sesssion = URLSession.shared
+			internal var sesssion: NetworkSession = URLSession.shared
 			
+			/// Calls to the live internet to retrieve Data from a specific location
+			/// - Parameters:
+			///   - url: The location you wish to fetch data from
+			///   - completionHandler: Returns a result object which signifies the status of the request
 			public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
-				let task = sesssion.dataTask(with: url) { data, response, error in
+				sesssion.get(from: url) { data, error in
 					let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
 					completionHandler(result)
 				}
-				task.resume()
 			}
 		}
 		
